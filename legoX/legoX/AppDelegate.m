d9f20df6c2b2b8faec5118f051cc53a13199191f
xiewujun <xiewujun@thedream.cc>
2020-07-29 17:47:11 +0800

    加入注释

--- legoX/legoX/AppDelegate.m
+++ legoX/legoX/AppDelegate.m
@@ -21,10 +21,29 @@
     
     [self initPFSDK];
     
-    [self setupWindow];
+    // 1>.设置主控制器
+    /*
+     1.主流框架 - 大家都这样写
+     2.非主流框架/！！！不可使用！！！ - UIWindow -> UINavigationController -> UITabBarController -> ChildViewControllers
+     */
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    LGTabBarController *tab_VC = [[LGTabBarController alloc] init];
+    self.window.rootViewController = tab_VC;
+    [self.window makeKeyAndVisible];
     
-    [self setupGuideAndAdPage];
-
+    // 2>.设置引导页/广告页
+    /**
+     1.并不是每次都需要显示引导页/怎么判断？？？
+     Q.因为每次判断都是固定的，所以我们可以把这个判断封装起来。新建一个LGAppInitSetting.swift文件
+     2.怎么让引导页显示在最上面？？？
+     Q.可以让他显示在临时UIWindow上面/！！！要求对UIWindow有一个比较深刻的理解！！！
+     */
+    if ([LGAppInitSetting shouldShowGuidePage]) {
+        LGGuideViewController *guide_VC = [[LGGuideViewController alloc]init];
+        // FIXME - 这里必须要求对UIWindow有一个比较深刻的理解
+    }
+    
     return YES;
 }
 
@@ -38,29 +57,6 @@
 // 初始化第三方
 -(void)initPFSDK {
     
-}
-
-// 设置主控制器
--(void)setupWindow {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
-    LGTabBarController *tab_VC = [[LGTabBarController alloc] init];
-    LGNavigationController *nav_VC = [[LGNavigationController alloc]initWithRootViewController:tab_VC];
-    self.window.rootViewController = nav_VC;
-    [self.window makeKeyAndVisible];
-}
-
-// 设置引导页/广告页
--(void)setupGuideAndAdPage {
-    if ([LGAppInitSetting shouldShowGuidePage]) {
-//        LGGuideViewController *guide_VC = [[LGGuideViewController alloc]init];
-    } else if ([LGAppInitSetting shouldShowAdPage]) {
-        
-    } else {
-        if ([LGUserManager isLogin]) {
-            // 显示新手引导
-        }
-    }
 }
 
 @end
